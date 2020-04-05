03ebd345b7874eb69451dc2999dcd9b08f3c916d
Gisela Zhunio <Karoz14@giselas-mbp.lan>
2020-04-05 15:33:05 -0400

    Added q

--- Q4.java
+++ Q4.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+
 public class Q4 {
     public static void main (String[] args){
+        ArrayList<String>names = new ArrayList<>();
 
+        names.add("Ivan");
+        names.add("Elita");
+        names.add("Karo");
+        names.add("Juan");
+
+        System.out.println(names);
+        System.out.println(names.size());
     }
 
 }
---
+++ Q4a.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Q4a {
+
+    public static void main(String[] args) {
+        ArrayList<Double> numbers = new ArrayList<>();
+
+        int n = 5;
+        for(int i = 0; i < n; i++){
+            double number = random(0,100);
+            numbers.add(number);
+        }
+
+        System.out.println(numbers);
+    }
+    public static double random (int min, int max ){
+        return min + (max - min) * Math.random();
+    }
+
+}
+
